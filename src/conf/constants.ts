214985c9d3d8a3d83c10f0e7dc9bb3bf734ec24a
nitives <cadenrichards15@gmail.com>
2026-02-10 00:48:09 -0500

    icons, readme, and metadata/seo

---
+++ src/conf/constants.ts
@@ -0,0 +1,46 @@
+export const APP = {
+  NAME: "22archive",
+  DESCRIPTION: "an unofficial 2hollis archive.",
+  AUTHOR: {
+    NAME: "nitves",
+    GITHUB: "",
+    TWITTER: "",
+  },
+};
+
+const KEYWORDS = [
+  "2hollis",
+  "archive",
+  "white tiger",
+  "unreleased",
+  "unreleased 2hollis",
+  "leaks",
+  "unreleased songs",
+  "discography",
+  "all songs",
+  "hyperpop",
+  "music archive",
+  "deleted songs",
+  "rarities",
+  "demos",
+  "chainmail",
+  "qiyoku",
+  "the jarl",
+  "boy album",
+  "star album",
+  "engineboye",
+  "live performances",
+  "music videos",
+  "underground music",
+  "glitchcore",
+  "trance music",
+];
+
+export const SEO = {
+  KEYWORDS: genKeywords({ keywords: KEYWORDS }),
+};
+
+function genKeywords({ keywords }: { keywords: string[] }): string[] {
+  const newKws = keywords.map((kw) => `2hollis ${kw}`);
+  return [...keywords, ...newKws];
+}
